256e189b2af5e9b54428a49919592a657e8b8e6d
vladzo <vashchukvlad17@gmail.com>
2025-03-16 17:24:51 +0200

    add partials

--- index.php
+++ index.php
@@ -1,27 +1,5 @@
 <?php
 
-$books = [
-    [
-        'title' => 'The Adventures of Tom Sawyer',
-        'author' => 'Mark Twain',
-        'price' => 200
-    ],
-    [
-        'title' => 'The Adventures of Sherlock Holmes',
-        'author' => 'Arthur Conan Doyle',
-        'price' => 250
-    ],
-    [
-        'title' => 'The Adventures of Huckleberry Finn',
-        'author' => 'Mark Twain',
-        'price' => 300
-    ]
-];
+$heading = "Home page";
 
-
-$filteredBooks = array_filter($books, function($book) {
-    return $book['price'] > 200;
-});
-
-
-require "index.view.php";
+require "views/index.view.php";
