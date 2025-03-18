8c69d7d9ae180e51e6895b1287190df7165e238c
vladzo <vashchukvlad17@gmail.com>
2025-03-18 15:30:51 +0200

    add db connection

--- index.php
+++ index.php
@@ -2,3 +2,16 @@
 
 require 'functions.php';
 require 'router.php';
+
+// Connect to database.
+$dsn = "sqlite:/home/vlad/projects/demo_php/demo_db";
+ 
+// Tip: This should be wrapped in a try-catch. We'll learn how, soon.
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$post = $statement->fetch(PDO::FETCH_ASSOC);
+
+echo "<li>" . $post['title'] . "</li>";
