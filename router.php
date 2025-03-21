3920dc25d2ec811bdca4f409d411b2d497014d2b
vladzo <vashchukvlad17@gmail.com>
2025-03-22 00:18:08 +0200

    Add note-create view

--- router.php
+++ router.php
@@ -1,14 +1,6 @@
 <?php
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/notes' => 'controllers/notes.php',
-    '/note' => 'controllers/note.php',
-    '/contact' => 'controllers/contact.php',
-];
+$routes = require 'routes.php';
 
 function routeToController($uri, $routes) {
     if (array_key_exists($uri, $routes)) {
@@ -18,5 +10,6 @@ function routeToController($uri, $routes) {
     }
 }
 
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 routeToController($uri, $routes);
